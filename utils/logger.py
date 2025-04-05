7e368d8edcbf8a1d657a75ca612b8373d893ca40
ARUL MURUGA A D <arulmuruga.ad@gmail.com>
2025-04-05 10:40:13 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def configure_logger(name, log_file='trading.log'):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    print(f"Logger {name} configured with file {log_file}")
+    return logger
