39f68dcebc8eb266f5c7ca8703514d5c5a465c16
ARUL MURUGA A D <arulmuruga.ad@gmail.com>
2025-04-05 10:35:18 +0530

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,17 @@
+API_CREDENTIALS = {
+    "api_key": "YOUR_API_KEY",
+    "access_token": "YOUR_ACCESS_TOKEN"
+}
+
+TRADE_CONFIG = {
+    "max_sells": 2,
+    "profit_threshold": 0.25,
+    "sl_trigger": 0.9,
+    "vix_threshold": 30.0,
+    "lot_size": 75,
+    "max_retries": 3,
+    "underlying": "NIFTY",
+    "vix_symbol": "INDIAVIX",
+    "product_type": "MIS",
+    "exchange": "NFO"
+}
