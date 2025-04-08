eddbcf193ae322c9cfd7774454717db9d72b2756
ARUL MURUGA A D <arulmuruga.ad@gmail.com>
2025-04-08 20:53:50 +0530

    Create instruments.py

---
+++ V3/instruments.py
@@ -0,0 +1,35 @@
+%%writefile instruments.py
+import pickle
+import os
+import requests
+from kiteconnect import KiteConnect
+from config import API_KEY
+
+class InstrumentManager:
+    def __init__(self):
+        self.instruments = {}
+        self.nifty_instruments = []
+        self.load_instruments()
+        
+    def load_instruments(self):
+        if os.path.exists('instruments.pkl'):
+            with open('instruments.pkl', 'rb') as f:
+                self.instruments = pickle.load(f)
+        else:
+            kite = KiteConnect(api_key=API_KEY)
+            all_instruments = kite.instruments()
+            self.nifty_instruments = [
+                i for i in all_instruments 
+                if i['tradingsymbol'].startswith('NIFTY') 
+                and i['segment'] == 'NFO-OPT'
+            ]
+            self.instruments = {i['instrument_token']: i for i in self.nifty_instruments}
+            with open('instruments.pkl', 'wb') as f:
+                pickle.dump(self.instruments, f)
+    
+    def get_instrument(self, instrument_token):
+        return self.instruments.get(instrument_token)
+    
+    def get_spot_price(self):
+        kite = KiteConnect(api_key=API_KEY)
+        return kite.ltp('NSE:NIFTY 50')['NSE:NIFTY 50']['last_price']
