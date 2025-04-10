a14e3b56ec028d3c5f29787b938d15e2eaa7415c
ARUL MURUGA A D <arulmuruga.ad@gmail.com>
2025-04-10 15:00:21 +0530

    Update instruments.py

--- V3/instruments.py
+++ V3/instruments.py
@@ -4,13 +4,15 @@ import os
 import requests
 from kiteconnect import KiteConnect
 from config import API_KEY
+from tenacity import retry, stop_after_attempt
 
 class InstrumentManager:
     def __init__(self):
         self.instruments = {}
         self.nifty_instruments = []
         self.load_instruments()
-        
+
+    @retry(stop=stop_after_attempt(3))
     def load_instruments(self):
         if os.path.exists('instruments.pkl'):
             with open('instruments.pkl', 'rb') as f:
